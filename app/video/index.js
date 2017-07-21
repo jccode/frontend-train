3f76c06bd2c3c0663a9b958b16ec5b565d57c3d6
jessie <junchangchen@gmail.com>
2017-07-21 10:05:32 +0800

    videojs playlist

--- app/video/index.js
+++ app/video/index.js
@@ -15,5 +15,38 @@ jQuery($ => {
 
 function init() {
     console.log("video");
+    play_video();
 }
+
+function play_video() {
+    const player = videojs("my-video");
+
+    const source = {
+        flv: {
+            src: "images/1.flv",
+            type: "video/x-flv"
+        },
+        mp4: {
+            src: "images/2.mp4",
+            type: "video/mp4"
+        }
+    };
+    
+    // player.src(source.mp4);
+    // player.play();
+
+    
+    const playlist = [
+        {sources: [source.flv]},
+        {sources: [source.mp4]}
+    ];
+    player.playlist(playlist);
+    player.playlist.autoadvance(0);
+    player.playlist.repeat(false);
+
+    // init playlist-ui
+    // player.playlistUi();
+
+}
+
 
